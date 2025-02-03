83ad98e1a7e9aeb645a519f3259fe54d9b2416e7
SyedAnees21 <syedanees816@gmail.com>
2025-02-03 17:19:53 +0500

    update: view functionalities

--- nft-auction/src/lib.rs
+++ nft-auction/src/lib.rs
@@ -41,6 +41,7 @@ use state::{AuctionState, ItemState, State};
 pub mod error;
 pub mod params;
 pub mod state;
+pub mod view;
 
 /// Contract token ID type. It has to be the `ContractTokenId` from the cis2
 /// token contract.
@@ -388,42 +389,105 @@ fn auction_cancel(ctx: &ReceiveContext, host: &mut Host<State>) -> ContractResul
     Ok(())
 }
 
-/// View function that returns the content of the state.
-#[receive(
-    contract = "cis2-auction",
-    name = "view",
-    return_value = "ReturnParamView"
-)]
-fn view(_ctx: &ReceiveContext, host: &Host<State>) -> ContractResult<ReturnParamView> {
-    let state = host.state();
+// /// View function that returns the content of the state.
+// #[receive(
+//     contract = "cis2-auction",
+//     name = "view",
+//     return_value = "ReturnParamView"
+// )]
+// fn view(_ctx: &ReceiveContext, host: &Host<State>) -> ContractResult<ReturnParamView> {
+//     let state = host.state();
 
-    let inner_state = state.items.iter().map(|x| (*x.0, x.1.clone())).collect();
+//     let inner_state = state.items.iter().map(|x| (*x.0, x.1.clone())).collect();
 
-    Ok(ReturnParamView {
-        item_states: inner_state,
-        counter: host.state().counter,
-    })
-}
+//     Ok(ReturnParamView {
+//         item_states: inner_state,
+//         counter: host.state().counter,
+//     })
+// }
 
-/// ViewItemState function that returns the state of a specific item.
-#[receive(
-    contract = "cis2-auction",
-    name = "viewItemState",
-    return_value = "ItemState",
-    parameter = "u16",
-    error = "Error"
-)]
-fn view_item_state(ctx: &ReceiveContext, host: &Host<State>) -> ContractResult<ItemState> {
-    // Getting input parameter.
-    let item_index: u16 = ctx.parameter_cursor().get()?;
-    let item = host
-        .state()
-        .items
-        .get(&item_index)
-        .map(|x| x.to_owned())
-        .ok_or(Error::NoItem)?;
-    Ok(item)
-}
+// /// ViewItemState function that returns the state of a specific item.
+// #[receive(
+//     contract = "cis2-auction",
+//     name = "viewItemState",
+//     return_value = "ItemState",
+//     parameter = "u16",
+//     error = "Error"
+// )]
+// fn view_item_state(ctx: &ReceiveContext, host: &Host<State>) -> ContractResult<ItemState> {
+//     // Getting input parameter.
+//     let item_index: u16 = ctx.parameter_cursor().get()?;
+//     let item = host
+//         .state()
+//         .items
+//         .get(&item_index)
+//         .map(|x| x.to_owned())
+//         .ok_or(Error::NoItem)?;
+//     Ok(item)
+// }
+
+// /// ViewItemState function that returns the state of a specific item.
+// #[receive(
+//     contract = "cis2-auction",
+//     name = "viewActive",
+//     return_value = "ReturnParamView",
+// )]
+// fn view_active(_ctx: &ReceiveContext, host: &Host<State>) -> ContractResult<ReturnParamView> {
+//     // Getting input parameter.
+//     let inner = get_items(host, AuctionState::NotSoldYet);
+    
+//     let count = inner.iter().count() as u16;
+
+//     Ok(ReturnParamView {
+//         item_states: inner,
+//         counter: count
+//     })
+// }
+
+// /// ViewItemState function that returns the state of a specific item.
+// #[receive(
+//     contract = "cis2-auction",
+//     name = "viewCanceled",
+//     return_value = "ReturnParamView",
+// )]
+// fn view_canceled(_ctx: &ReceiveContext, host: &Host<State>) -> ContractResult<ReturnParamView> {
+//     // Getting input parameter.
+//     let inner = get_items(host, AuctionState::Canceled);
+    
+//     let count = inner.iter().count() as u16;
+
+//     Ok(ReturnParamView {
+//         item_states: inner,
+//         counter: count
+//     })
+// }
+
+// /// ViewItemState function that returns the state of a specific item.
+// #[receive(
+//     contract = "cis2-auction",
+//     name = "viewFinalized",
+//     return_value = "ReturnParamView",
+// )]
+// fn view_finalized(_ctx: &ReceiveContext, host: &Host<State>) -> ContractResult<ReturnParamView> {
+//     // Getting input parameter.
+//     let inner = host
+//         .state()
+//         .items
+//         .iter()
+//         .filter(|(_, item)| {
+//             item.auction_state != AuctionState::NotSoldYet &&
+//             item.auction_state != AuctionState::Canceled
+//         })
+//         .map(|(index, item)| (*index, item.clone()))
+//         .collect::<Vec<_>>();
+
+//     let count = inner.iter().count() as u16;
+
+//     Ok(ReturnParamView {
+//         item_states: inner,
+//         counter: count
+//     })
+// }
 
 /// Helper function that can be invoked at the front-end to serialize the
 /// `AdditionalDataIndex` before generating the message to be signed in the
@@ -476,3 +540,17 @@ fn ensure_is_operator(
     }
     Ok(())
 }
+
+// fn get_items(host: &Host<State>, state: AuctionState) -> Vec<(u16, ItemState)> {
+//     let inner = host
+//         .state()
+//         .items
+//         .iter()
+//         .filter(|(_, item)| {
+//             item.auction_state == state
+//         })
+//         .map(|(index, item)| (*index, item.clone()))
+//         .collect::<Vec<_>>();
+
+//     inner
+// }
